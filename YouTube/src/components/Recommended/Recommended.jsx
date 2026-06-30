304f1e501d68dbcd0911572f06971b6d7fe06685
Piyush Shah <piyush720shah@gmail.com>
2026-06-30 18:27:22 +0530

    Add Recommended Video section in YouTube

--- YouTube/src/components/Recommended/Recommended.jsx
+++ YouTube/src/components/Recommended/Recommended.jsx
@@ -1,8 +1,71 @@
-import "./Recommended.css"
-const Recommended = () => {
-  return (
-    <div>Recommended</div>
-  );
-}
+import "./Recommended.css";
+import thumbnail from "../../assets/DB08.jpg";
 
-export default Recommended
+const Recommended = () => {
+	return (
+		<div className="recommended-video">
+			<div className="video-card">
+				<img src={thumbnail} alt="" />
+				<div className="video-details">
+					<h3>
+						Title Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+					</h3>
+          <h2>Chai Aur Code</h2>
+          <p>15K views &bull; 22 hours ago</p>
+				</div>
+			</div>
+			<div className="video-card">
+				<img src={thumbnail} alt="" />
+				<div className="video-details">
+					<h3>
+						Title Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+					</h3>
+          <h2>Chai Aur Code</h2>
+          <p>15K views &bull; 22 hours ago</p>
+				</div>
+			</div>
+			<div className="video-card">
+				<img src={thumbnail} alt="" />
+				<div className="video-details">
+					<h3>
+						Title Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+					</h3>
+          <h2>Chai Aur Code</h2>
+          <p>15K views &bull; 22 hours ago</p>
+				</div>
+			</div>
+			<div className="video-card">
+				<img src={thumbnail} alt="" />
+				<div className="video-details">
+					<h3>
+						Title Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+					</h3>
+          <h2>Chai Aur Code</h2>
+          <p>15K views &bull; 22 hours ago</p>
+				</div>
+			</div>
+			<div className="video-card">
+				<img src={thumbnail} alt="" />
+				<div className="video-details">
+					<h3>
+						Title Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+					</h3>
+          <h2>Chai Aur Code</h2>
+          <p>15K views &bull; 22 hours ago</p>
+				</div>
+			</div>
+			<div className="video-card">
+				<img src={thumbnail} alt="" />
+				<div className="video-details">
+					<h3>
+						Title Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+					</h3>
+          <h2>Chai Aur Code</h2>
+          <p>15K views &bull; 22 hours ago</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Recommended;
