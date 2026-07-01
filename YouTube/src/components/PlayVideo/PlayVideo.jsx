13043f1325a2e775040f143ed812f8b259bb9946
Piyush Shah <piyush720shah@gmail.com>
2026-07-01 16:08:02 +0530

    Add recommended videos in YouTube

--- YouTube/src/components/PlayVideo/PlayVideo.jsx
+++ YouTube/src/components/PlayVideo/PlayVideo.jsx
@@ -1,19 +1,13 @@
 import "./PlayVideo.css";
-import video1 from "../../assets/video1.mp4";
 import like from "../../assets/like.png";
 import dislike from "../../assets/dislike.png";
 import save from "../../assets/save.svg";
 import share from "../../assets/share.svg";
-import userProfile from "../../assets/profile_icon.svg";
-import notifications from "../../assets/notifications_iocn.svg";
-import greaterThan from "../../assets/gt.svg";
-import greatStack from "../../assets/great_stack.png";
-import more from "../../assets/more.svg"
+import more from "../../assets/more.svg";
 import { API_KEY, valueConverter } from "../../data";
 import { useEffect, useState } from "react";
 
 const PlayVideo = ({ videoId }) => {
-
 	const [apiData, setApiData] = useState(null);
 	const [channelData, setChannelData] = useState(null);
 
@@ -22,14 +16,14 @@ const PlayVideo = ({ videoId }) => {
 		await fetch(videoDetails_url)
 			.then((response) => response.json())
 			.then((data) => setApiData(data.items[0]));
-	}
+	};
 
 	const fetchChannelData = async () => {
 		const channelData_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`;
 		await fetch(channelData_url)
 			.then((response) => response.json())
 			.then((data) => setChannelData(data.items[0]));
-	}
+	};
 
 	useEffect(() => {
 		fetchVideoData();
@@ -43,37 +37,38 @@ const PlayVideo = ({ videoId }) => {
 
 	return (
 		<div className="play-video flex-div">
-			{/* <video src={video1} controls autoPlay></video> */}
 			<iframe
 				src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
-				frameborder="0"
+				frameBorder="0"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-				referrerpolicy="strict-origin-when-cross-origin"
-				allowfullscreen></iframe>
-			<h3>{apiData?apiData.snippet.title:"Title Here"}</h3>
+				referrerPolicy="strict-origin-when-cross-origin"
+				allowFullScreen></iframe>
+			<h3>{apiData ? apiData.snippet.title : "Title Here"}</h3>
 			<div className="channel-stats flex-div">
 				<div className="stats-left flex-div">
-					<img className="channel-logo" src={channelData?channelData.snippet.thumbnails.default.url:""} alt="" />
+					<img
+						className="channel-logo"
+						src={
+							channelData ? channelData.snippet.thumbnails.default.url : null
+						}
+						alt=""
+					/>
 					<div>
-						<h2>{apiData?apiData.snippet.channelTitle:"Channel Name"}</h2>
-						<p>{channelData?valueConverter(channelData.statistics.subscriberCount):"0"} Subscribers</p>
+						<h2>{apiData ? apiData.snippet.channelTitle : "Channel Name"}</h2>
+						<p>
+							{channelData
+								? valueConverter(channelData.statistics.subscriberCount)
+								: "0"}{" "}
+							Subscribers
+						</p>
 					</div>
 					<button>Join</button>
 					<button>Subscribe</button>
-					{/* <div className="notification-icon flex-div">
-						<img src={notifications} alt="" />
-						<img
-							className="greater-than-icon"
-							style={{ width: "12px", marginLeft: "10px" }}
-							src={greaterThan}
-							alt=""
-						/>
-					</div> */}
 				</div>
 				<div className="stats-right flex-div">
 					<div className="like">
 						<img src={like} alt="" />
-						{valueConverter(apiData?apiData.statistics.likeCount:0)}
+						{valueConverter(apiData ? apiData.statistics.likeCount : 0)}
 					</div>
 					<div className="dislike">
 						<img src={dislike} alt="" />
--- YouTube/src/components/Recommended/Recommended.jsx
+++ YouTube/src/components/Recommended/Recommended.jsx
@@ -1,69 +1,43 @@
 import "./Recommended.css";
-import thumbnail from "../../assets/DB08.jpg";
+import { useEffect, useState } from "react";
+import { API_KEY, valueConverter } from "../../data";
+import moment from "moment";
+import { Link } from "react-router-dom";
 
-const Recommended = ({categoryId}) => {
+const Recommended = ({ categoryId }) => {
+	const [apiData, setApiData] = useState([]);
+
+	const fetchData = async () => {
+		const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&videoCategoryId=${categoryId}&key=${API_KEY}`;
+		await fetch(relatedVideo_url)
+			.then((res) => res.json())
+			.then((data) => setApiData(data.items));
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, [categoryId]);
+
 	return (
 		<div className="recommended-video">
-			<div className="video-card">
-				<img src={thumbnail} alt="" />
-				<div className="video-details">
-					<h3>
-						Title Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-					</h3>
-          <h2>Chai Aur Code</h2>
-          <p>15K views &bull; 22 hours ago</p>
-				</div>
-			</div>
-			<div className="video-card">
-				<img src={thumbnail} alt="" />
-				<div className="video-details">
-					<h3>
-						Title Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-					</h3>
-          <h2>Chai Aur Code</h2>
-          <p>15K views &bull; 22 hours ago</p>
-				</div>
-			</div>
-			<div className="video-card">
-				<img src={thumbnail} alt="" />
-				<div className="video-details">
-					<h3>
-						Title Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-					</h3>
-          <h2>Chai Aur Code</h2>
-          <p>15K views &bull; 22 hours ago</p>
-				</div>
-			</div>
-			<div className="video-card">
-				<img src={thumbnail} alt="" />
-				<div className="video-details">
-					<h3>
-						Title Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-					</h3>
-          <h2>Chai Aur Code</h2>
-          <p>15K views &bull; 22 hours ago</p>
-				</div>
-			</div>
-			<div className="video-card">
-				<img src={thumbnail} alt="" />
-				<div className="video-details">
-					<h3>
-						Title Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-					</h3>
-          <h2>Chai Aur Code</h2>
-          <p>15K views &bull; 22 hours ago</p>
-				</div>
-			</div>
-			<div className="video-card">
-				<img src={thumbnail} alt="" />
-				<div className="video-details">
-					<h3>
-						Title Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-					</h3>
-          <h2>Chai Aur Code</h2>
-          <p>15K views &bull; 22 hours ago</p>
-				</div>
-			</div>
+			{apiData.map((item, index) => {
+				return (
+					<Link
+						to={`/video/${item.snippet.categoryId}/${item.id}`}
+						key={index}
+						className="video-card">
+						<img src={item.snippet.thumbnails.medium.url} alt="" />
+						<div className="video-details">
+							<h3>{item.snippet.title}</h3>
+							<h2>{item.snippet.channelTitle}</h2>
+							<p>
+								{valueConverter(item.statistics.viewCount)} views &bull;{" "}
+								{moment(item.snippet.publishedAt).fromNow()}
+							</p>
+						</div>
+					</Link>
+				);
+			})}
 		</div>
 	);
 };
